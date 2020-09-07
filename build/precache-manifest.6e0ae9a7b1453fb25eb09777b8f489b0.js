15ddae344a7aacfdeff3a005c9791be7f0124405
boamahep <boamahep@gmail.com>
2020-09-07 21:13:48 +0000

    new

---
+++ build/precache-manifest.6e0ae9a7b1453fb25eb09777b8f489b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9138aa440b6718021b5f6987dae46a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4a3cd7f3435e44cb9d",
+    "url": "/static/css/main.0a0650df.chunk.css"
+  },
+  {
+    "revision": "d2e373c254585c9be00e",
+    "url": "/static/js/2.a7f2996d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7f2996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4a3cd7f3435e44cb9d",
+    "url": "/static/js/main.95c2d01a.chunk.js"
+  },
+  {
+    "revision": "fc67fad158ce2762ef25",
+    "url": "/static/js/runtime-main.02b54de1.js"
+  },
+  {
+    "revision": "6c5827cc271d60c0b21a94f2090910f2",
+    "url": "/static/media/logo.6c5827cc.PNG"
+  }
+]);

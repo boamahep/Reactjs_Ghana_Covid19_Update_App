56ea34c1bbc2808ac1741c1e76dd66c5d57f9e20
boamahep <boamahep@gmail.com>
2020-09-07 22:09:33 +0000

    Updates

---
+++ precache-manifest.d27774e7924a209503b45e4ba9868c31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b836ddff25c2fb2e6ccada4ef0b4f1",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/index.html"
+  },
+  {
+    "revision": "2ce92a2e480f53b35b12",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/static/css/main.0a0650df.chunk.css"
+  },
+  {
+    "revision": "d2e373c254585c9be00e",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/static/js/2.a7f2996d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/static/js/2.a7f2996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce92a2e480f53b35b12",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/static/js/main.0e4f62c5.chunk.js"
+  },
+  {
+    "revision": "87805a700fe3aa273d4a",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/static/js/runtime-main.fb43423c.js"
+  },
+  {
+    "revision": "6c5827cc271d60c0b21a94f2090910f2",
+    "url": "/Reactjs_Ghana_Covid19_Update_App/static/media/logo.6c5827cc.PNG"
+  }
+]);
